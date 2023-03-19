49b1677ac2922080bc41fc6c34a2603d25414332
prabutama <isaprabutama@gmail.com>
2023-03-19 10:23:55 +0700

    Initial commit

---
+++ src/com/isa/main.java
@@ -0,0 +1,13 @@
+package com.isa;
+
+public class main {
+
+    // args = nilai setelah main
+    // kalian bisa coba melalui terminal
+
+    public static void main(String[] args) {
+        System.out.println("hallo " + args[0] + " ganteng");
+        System.out.println("hallo " + args[1] + " manis");
+        System.out.println("hallo " + args[2] + " kecil");
+    };
+};
